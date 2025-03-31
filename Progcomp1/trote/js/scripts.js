a724b25a006cf5c09253226301c41a9e43c53b80
CASSIANO CARDOSO DE FIGUEIREDO <26475@unifacef.edu.br>
2025-03-31 20:32:36 -0300

    site trote atualizado

--- Progcomp1/trote/js/scripts.js
+++ Progcomp1/trote/js/scripts.js
@@ -6,9 +6,26 @@ function calcular(){
    soma = soma + Number(homenagem)
    let leite = document.getElementById("leite").value
    soma = soma + Number(2 * Number(leite))
-   let kitAvulso = document.getElementById("kitAvulso").value
-   soma = soma + Number(30 * Number(kitAvulso))
-   let suplementoAvulso = document.getElementById("suplementoAvulso").value
-   soma = soma + Number(15 * Number(suplementoAvulso))
+   let kit = Number(document.getElementById("kit").value)
+   let pontoskit = 0
+   let equipe = document.getElementById("equipe").value
+   if(equipe) == ("laranja"){
+      if(Number(kit) >= 97){
+         pontosKit = 5000 + ((kit - 97) * 30)
+      }
+      else if (kit >= 78){
+         pontosKit = 4000 + ((kit - 78) * 30)
+      }
+      else if (kit >= 49) {
+         pontosKit = 2500 + ((kit - 19) *30)
+      }
+      else if (kit >=19) {
+         pontoskit = 1000 + ((kit - 19)* 30)
+      }
+   }
+   soma = soma + Number(30 * Number(kit))
+   let suplemento = document.getElementById("suplemento").value
+   soma = soma + Number(15 * Number(suplemento))
    document.getElementById("soma").innerHTML = soma.toFixed(2)
+
 }
